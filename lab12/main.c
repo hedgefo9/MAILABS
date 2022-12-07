d75730e7bee17fb3a8a7d50ab28a4234e643897c
hedgefo9 <113327578+hedgefo9@users.noreply.github.com>
2022-12-07 17:09:54 +0300

    Add files via upload

--- lab12/main.c
+++ lab12/main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <assert.h>
 
-#define INT_MIN -2147483648
+#define INT_MIN (-2147483648)
 #define INT_MAX 2147483647
 
 // возвращает модуль числа
@@ -24,15 +24,21 @@ int sign(int a) {
     }
 }
 
+// возвращает количество разрядов у числа
 int digits_quantity(int x) {
+    int x_abs = abs(x);
+    if (x_abs == 0) {
+        return 1;
+    }
     int num_len = 0;
-    while (x > 0) {
+    while (x_abs > 0) {
         num_len++;
-        x = x / 10;
+        x_abs = x_abs / 10;
     }
     return num_len;
 }
 
+// возвращает число, равное x возведённому в положительную или нулевую степень power
 int pow_int(int x, int power) {
     int res = 1;
     for (int i = 0; i < power; i++) {
@@ -41,17 +47,15 @@ int pow_int(int x, int power) {
     return res;
 }
 
-int shift_left(int x, int x_len, int shift_val) {
+// возвращает число, равное сдвинутому числу x на shift_val влево
+int shift_left(int x, int shift_val) {
     int new_num = x;
+    int x_len = digits_quantity(x);
+    shift_val = shift_val % x_len;
+    int y = pow_int(10, x_len - 1);
     for (int k = 0; k < shift_val; k++) {
         int x_tmp = new_num;
-        new_num = 0;
-        for (int i = 0; i < x_len - 1; i++) {
-            new_num = new_num + (x_tmp % 10) * pow_int(10, digits_quantity(new_num));
-            x_tmp /= 10;
-        }
-        int first_digit = x_tmp;
-        new_num = new_num * 10 + first_digit;
+        new_num = (x_tmp % y) * 10 + (x_tmp / y);
     }
     return new_num;
 }
@@ -68,9 +72,33 @@ void test_sign() {
     assert(sign(0) == 0);
 }
 
+void test_digits_quantity() {
+    assert(digits_quantity(0) == 1);
+    assert(digits_quantity(1234) == 4);
+    assert(digits_quantity(348584739) == 9);
+    assert(digits_quantity(-10000) == 5);
+}
+
+void test_pow_int() {
+    assert(pow_int(10, 4) == 10000);
+    assert(pow_int(2, 20) == 1048576);
+    assert(pow_int(100, 0) == 1);
+    assert(pow_int(-5, 2) == 25);
+    assert(pow_int(-7, 5) == -16807);
+}
+
+void test_shift_left() {
+    assert(shift_left(1234, 1) == 2341);
+    assert(shift_left(230503604, 2) == 50360423);
+    assert(shift_left(9284158, 5764801) == 9284158);
+}
+
 void test_all() {
     test_abs();
     test_sign();
+    test_digits_quantity();
+    test_pow_int();
+    test_shift_left();
 }
 
 int main() {
@@ -87,13 +115,11 @@ int main() {
         return 0;
     }
 
+    // сохранение знака
     int sign_original = sign(num);
     int num_abs = abs(num);
 
-    // подсчет количества разрядов
-    int num_len = digits_quantity(num_abs);
-
-    int shifted_num = shift_left(num_abs, num_len, shift_value % num_len) * sign_original;
+    int shifted_num = shift_left(num_abs, shift_value) * sign_original;
     printf("%d\n", shifted_num);
 
     return 0;
